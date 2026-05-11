895662c42d071dfd075925016c05a7503fab7754
shs_leOne <2245461389@qq.com>
2026-05-11 19:27:30 +0800

    1

---
+++ AvaloniaAppDemo/Views/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AvaloniaAppDemo.ViewModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaAppDemo.views;
+
+public partial class MainWindowViewModel : BaseViewModel
+{
+    [ObservableProperty] private string _title = "Hello World";
+
+    public MainWindowViewModel()
+    {
+        
+    }
+}
