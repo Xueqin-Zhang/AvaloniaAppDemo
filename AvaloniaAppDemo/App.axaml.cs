a4ef7bb236bfadb156edcde656a0b2fcb03eeed7
shs_leOne <2245461389@qq.com>
2026-05-11 15:51:17 +0800

    11

--- AvaloniaAppDemo/App.axaml.cs
+++ AvaloniaAppDemo/App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using AvaloniaAppDemo.views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AvaloniaAppDemo {
     public partial class App : Application {
@@ -8,15 +10,25 @@ namespace AvaloniaAppDemo {
         {
             AvaloniaXamlLoader.Load(this);
         }
-
+        
         public override void OnFrameworkInitializationCompleted()
         {
+            var services = new ServiceCollection();
+            AddServices(services);
+            
+            var serviceProvider = services.BuildServiceProvider();
+            
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
+                desktop.MainWindow.DataContext = serviceProvider.GetRequiredService<MainWindowViewModel>();
             }
-
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void AddServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
         }
     }
 }
--- AvaloniaAppDemo/Program.cs
+++ AvaloniaAppDemo/Program.cs
@@ -1,16 +1,14 @@
 ﻿using Avalonia;
 using System;
 
-namespace AvaloniaAppDemo {
-    internal class Program {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
+namespace AvaloniaAppDemo
+{
+    internal class Program
+    {
         [STAThread]
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
